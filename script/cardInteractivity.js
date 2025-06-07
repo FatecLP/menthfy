0f073471c18b6dc7c5dd6d4d1b67cdbc71d0f1f7
Nickolas <nickolaslopes.developer@gmail.com>
2025-06-07 01:31:26 -0300

    feat: new images, new components and cnha

--- script/cardInteractivity.js
+++ script/cardInteractivity.js
@@ -4,3 +4,9 @@ for(let i = 0; i < card.length; i++){
         card[i].classList.add('classAnimation')
 })
 }
+let subject = document.querySelectorAll('.subject-component')
+for(let i = 0; i < subject.length; i++){
+    subject[i].addEventListener('mouseout', function animation(){
+        subject[i].classList.add('classAnimationSubject')
+})
+}
